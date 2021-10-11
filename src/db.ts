9d1d7554b6c8f2c6a49e18bdb324af6bbaf5a698
David <raymondinf23@gmail.com>
2021-10-11 00:54:33 -0400

    Add database connection

---
+++ src/db.ts
@@ -0,0 +1,9 @@
+import * as dotenv from 'dotenv';
+import { MongoClient } from 'mongodb';
+
+dotenv.config()
+const uri = process.env.MONGO_URI!;
+const client = new MongoClient(uri);
+client.connect().then( client => console.log('Database connected'));
+
+export default client;
